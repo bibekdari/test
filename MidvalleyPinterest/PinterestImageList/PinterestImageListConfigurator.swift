40b77785bbd6eb7285b545835e7da2921775b7b0
bibek timalsina <bibektimalsina@bibeks-MacBook-Pro.local>
2019-03-22 13:52:17 +0545

    added pinterest image configurator

---
+++ MidvalleyPinterest/PinterestImageList/PinterestImageListConfigurator.swift
@@ -0,0 +1,20 @@
+//
+//  PinterestImageListConfigurator.swift
+//  MidvalleyPinterest
+//
+//  Created by bibek timalsina on 3/22/19.
+//  Copyright © 2019 bibek timalsina. All rights reserved.
+//
+
+import UIKit
+
+class PinterestImageListConfigurator {
+    
+    func configuredViewController() -> PinterestViewController? {
+        let sb = UIStoryboard(name: "Pinterest", bundle: nil)
+        let viewController = sb.instantiateInitialViewController() as? PinterestViewController
+        viewController?.requestHandler = RequestHandlerImpl(baseURLString: "http://pastebin.com")
+        return viewController
+    }
+    
+}
