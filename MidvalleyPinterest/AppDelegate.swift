9c5311178d7473c1acede2aa86259235c76ce8ba
bibek timalsina <bibektimalsina@bibeks-MacBook-Pro.local>
2019-03-22 17:41:00 +0545

    changed way of creating root view controll of window

--- MidvalleyPinterest/AppDelegate.swift
+++ MidvalleyPinterest/AppDelegate.swift
@@ -16,7 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window?.rootViewController = PinterestImageListConfigurator().configuredViewController()
+        
+        let cacheManager = CacheManager.shared
+        cacheManager.size = 104857600
+        let taskManager = TaskManager(withCacheManager: cacheManager)
+        
+        window?.rootViewController = PinterestImageListConfigurator().configuredViewController(with: taskManager)
         return true
     }
 
