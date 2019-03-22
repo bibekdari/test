c1ffedf4a7733557f1a48c3b8f37a606ffb6208f
bibek timalsina <bibektimalsina@bibeks-MacBook-Pro.local>
2019-03-22 13:51:17 +0545

    created get posts api

---
+++ MidvalleyPinterest/RequestHandler/PostsAPI.swift
@@ -0,0 +1,34 @@
+//
+//  PostsAPI.swift
+//  MidvalleyPinterest
+//
+//  Created by bibek timalsina on 3/21/19.
+//  Copyright © 2019 bibek timalsina. All rights reserved.
+//
+
+import Foundation
+
+struct PostContainer: Decodable {
+    let posts: [Post]
+}
+
+protocol PostsAPI {
+    var requestHandler: RequestHandler? {get}
+}
+
+extension PostsAPI {
+    
+    func getPosts(success: @escaping ([Post]) -> (), failure: @escaping (Error) -> ()) throws {
+        let slug = "raw/wgkJgazE"
+        _ = try requestHandler?.request(slug: slug) { (response: Response<PostContainer>) in
+            switch response {
+            case .error(let error):
+                failure(error)
+            case .success(let model):
+                success(model.posts)
+            }
+        }
+    }
+    
+}
+
